b9b7ab4e686173ba78c0e5c5a25ecda82c3b16f9
alexxwav <cesar.martinez7584@alumnos.udg.mx>
2026-09-14 15:42:25 -0600

    Operaciones basicas
    se cambio el nombre de el archivo suma a operaciones básicas y se agregó la resta al menú

--- Menu.py
+++ Menu.py
@@ -1,10 +1,11 @@
-from Suma import sumar
+from operaciones_basicas import sumar, restar
 
 op = -1
 
 while op != 0:
     print("Menu:")
     print("1. Suma")
+    print("2. Resta")
     print("0. Salir")
     op = int(input("Seleccione la opcion deseada: "))
 
@@ -12,6 +13,10 @@ while op != 0:
         a = float(input("Digite el primer numero: "))
         b = float(input("Digite el segundo numero: "))
         print("Resultado:", sumar(a, b))
+    elif op == 2:
+        a = float(input("Digite el primer numero: "))
+        b = float(input("Digite el segundo numero: "))
+        print("Resultado:", restar(a, b))
     elif op == 0:
         print("Adios broo")
     else:
