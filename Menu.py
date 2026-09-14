9d93824dfe46c12584b994dff14dfd4fd8b59421
alexxwav <cesar.martinez7584@alumnos.udg.mx>
2026-09-14 15:30:36 -0600

    Menu

---
+++ Menu.py
@@ -0,0 +1,13 @@
+op = -1
+
+while op!=0:
+    print ("Menu:")
+    print ("1. Suma")
+    print ("0. Salir")
+    op= int(input("Seleccione la opcion deseada:"))
+
+if op==0:
+    print ("adios broo")
+
+else:
+    print ("Opcion no valida")
